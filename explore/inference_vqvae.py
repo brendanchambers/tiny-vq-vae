4c25ed75fb184985633ecede3fcd0130ddd25b55
brendan chambers <brendan.chambers@quillbot.com>
2024-09-08 13:00:21 -0400

    simple inference example on a randomly init vqvae

---
+++ explore/inference_vqvae.py
@@ -0,0 +1,148 @@
+# learn the absolute basics
+#   and get a sense for how slow cpu inference is with no optimizations
+
+from datasets import load_dataset
+from diffusers import VQModel
+
+# from timm.data.transforms_factory import create_transform
+# from torchvision import transforms
+import torch
+from torchvision.transforms import v2
+from dataclasses import dataclass
+import time
+
+####################
+# define some default values to get an exploration working
+
+@dataclass
+class QuickArgs:
+    hresolution: int = 64
+    wresolution: int = 64
+    center_crop: float = None
+    random_flip: bool = True
+    bsz: int = 2
+
+args = QuickArgs()
+print(args)
+print(f"x res: {args.wresolution}  y res: {args.hresolution}")
+
+
+#####################
+# quick arbitrary example to learn about typical preprocessing steps
+
+image_dataset = "jbarat/plant_species"
+dataset = load_dataset(image_dataset)
+print(dataset)
+
+# sanity
+print("dataset on load...")
+print(dataset['train'][0]['image'])  # {'image':, 'label':}
+# ds['train'][42]['image'].show()
+#    size of an example plants image: 
+        # 375 x 50
+
+
+####### preprocess
+# random augmentation taken from train_vqgan diffusers example
+
+# Preprocessing the datasets.
+example_transforms = v2.Compose([
+    v2.ToImage(),  # Convert to tensor, only needed if you had a PIL image
+    v2.Resize((args.wresolution, args.hresolution), interpolation=v2.InterpolationMode.BILINEAR),   
+    v2.ToDtype(torch.uint8, scale=True),  # optional, most input are already uint8 at this point
+    # ...
+    v2.RandomResizedCrop(size=(args.wresolution, args.hresolution), antialias=True),  # Or Resize(antialias=True)  (todo how does this interact with resize?)
+    # ...
+    v2.ToDtype(torch.float32, scale=True),  # Normalize expects float input
+    # v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+print('apply preprocessing transformations...')
+# print(dataset['train'][0])
+# dataset['train'][0]['image'].show()
+train_dataset = dataset["train"].with_transform(example_transforms)   # hmmm todo look closer at this
+
+print('after preprocessing...')
+# print(train_dataset)
+print(train_dataset[0]['image'].size())
+print(train_dataset[1]['image'].size())
+pil_image = v2.functional.to_pil_image(train_dataset[0]['image'])
+print(train_dataset[0]['image'].size())
+print(train_dataset[1]['image'].size())
+# pil_image.show()
+
+
+######## inference
+
+# tokenize the dataset using a default init'd vqvae model
+model = VQModel()
+print(f"\n\n\n\n{model}\n\n\n\n")
+#     size of the model inputs: 64
+
+
+
+def visualize_outs(outs, show=True):
+    """ warning this will fail for batched outputs """
+    print(outs)
+    print(type(outs))
+    print([k for k in outs])  # sample, commit_loss
+
+    print("sample and commit_loss size:")
+    print(f" sample dimensions: {outs['sample'].size()}")
+    print(f" commit_loss: {outs['commit_loss']}")
+
+    if show:
+        print('reconstructed sample:')
+        v2.functional.to_pil_image(outs['sample'].squeeze()).show()
+
+########### sanity chceck
+
+i_img = 42
+img = train_dataset[i_img]['image']
+batched_img = torch.unsqueeze(img, 0)  # add a batch dimension 
+print(img.size())
+print(batched_img.size())
+outs = model(batched_img)
+print('=============================')
+
+visualize_outs(outs[0,:])  # show the sample and loss
+
+
+######################################
+
+def yield_batches(itr, n=1):
+    l = len(itr)
+    for ndx in range(0, l, n):
+        yield itr[ndx:min(ndx + n, l)]
+
+def pack_batch(batch):
+
+    # handle singleton batch
+    if args.bsz==1:
+        image = batch['image'][0]
+        batched_tensor = image.unsqueeze(dim=0)
+    else:
+        images = batch['image']
+        batched_tensor = torch.stack(images, dim=0)
+
+    return batched_tensor
+
+################## run inference over the dataset
+
+t_s = time.time()
+
+for i_batch, batch_entries in enumerate(yield_batches(train_dataset, n=args.bsz)):
+    print(f"i batch: {i_batch}")
+    batched_tensor = pack_batch(batch_entries)
+
+    with torch.inference_mode():
+        outs = model(batched_tensor)
+
+t_e = time.time()
+print(f"elapsed (s): {t_e - t_s}")
+print(f"num images: {len(train_dataset)}")
+print(f"batchsize: {args.bsz}")
+print(f"images / s: {len(train_dataset) / (t_e - t_s)}")   # ~11 img/s bsz=1 cpu tiny-laptop
+                                                            # ~14 imgs/s bsz=2 cpu tiny-laptop
+
+
